3073c7e7ef2dd3f85e558d814368e919f3c40bda
proudbyte <fcc.soc@gmail.com>
2014-06-30 17:18:59 +0100

    Form changes applied

--- form.n2b.pt/client/app/js/app.js
+++ form.n2b.pt/client/app/js/app.js
@@ -13,7 +13,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/acerca', {templateUrl: '/app/partials/contacts.html', controller: 'acerca'});
+    $routeProvider.when('/acerca', {templateUrl: '/app/partials/acerca.html', controller: 'acerca'});
     $routeProvider.when('/contacts', {templateUrl: '/app/partials/contacts.html', controller: 'contacts'});
     $routeProvider.when('/upgrade', {templateUrl: '/app/partials/upgrade.html', controller: 'MyCtrl1'});
     $routeProvider.when('/transfer', {templateUrl: '/app/partials/transfer.html', controller: 'MyCtrl2'});
