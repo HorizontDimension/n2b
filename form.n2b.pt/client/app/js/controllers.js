1f62367e32b672fa7b743f88d12011b1a8b1ef9d
proudbyte <fcc.soc@gmail.com>
2014-06-30 17:17:23 +0100

    Form changes applied

--- form.n2b.pt/client/app/js/controllers.js
+++ form.n2b.pt/client/app/js/controllers.js
@@ -6,6 +6,9 @@ angular.module('myApp.controllers', [])
     .controller('MyCtrl1', ['$scope', 'Transfers', function ($scope) {
 
     }])
+    .controller('acerca', ['$scope', 'Transfers', function ($scope) {
+
+    }])
     .controller('contacts', ['$scope', 'Transfers', function ($scope) {
 
     }])
